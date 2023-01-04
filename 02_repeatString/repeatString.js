de01f3289e3cdb7220ec2502502fc2fdf8733f8a
lmfrench <lisa_french@hotmail.com>
2023-01-04 22:48:20 +0000

    Update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringStore = '';
+    if (num < 0){
+        stringStore ="ERROR";
+    } 
+    for (let i = 0; i < num; i++){
+        stringStore += string;
+    }
+    return stringStore;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
