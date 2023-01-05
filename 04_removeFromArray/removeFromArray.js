94a94f6fd1d041b3c8684f5eb9ede2d3457b544f
lmfrench <lisa_french@hotmail.com>
2023-01-05 21:12:56 +0000

    Update removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...removeArray) {
+    for (let element of removeArray){
+        let countArray = 0;
+        while (countArray < array.length){
+            if (array[countArray] === element){
+                array.splice(countArray, 1);
+                countArray++;
+            } else countArray++
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
