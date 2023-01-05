d656176da5415e5ab94ee0c75367f18145bd4887
lmfrench <lisa_french@hotmail.com>
2023-01-05 18:38:12 +0000

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    let newString = "";
+    let count = 0;
+    while (count < string.length){
+        newString = stringArray[count] + newString;
+        count ++;
+    }
 
-};
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
