eb565db885b53220ff0c9a5637c57fad794297d0
lmfrench <lisa_french@hotmail.com>
2023-01-11 20:49:24 +0000

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+   
+    if (Number.isInteger(firstInt) && Number.isInteger(secondInt)
+        && (firstInt >= 0) && (secondInt >= 0)){
+        
+        const largeInt = Math.max(firstInt,secondInt);
+        const smallInt = Math.min(firstInt,secondInt);
+        let sum = 0;
 
+        for (let i = smallInt; i<=largeInt; i++){
+            sum += i; 
+        }
+        return sum;
+
+    } else return "ERROR";
 };
 
 // Do not edit below this line
